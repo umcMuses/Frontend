93cc4c9633c24a39e039a237a93495d6875a8a92
Awesome/Lee seung chul <chulee503@naver.com>
2026-01-01 19:36:10 +0900

    feat: Header 구현 중

---
+++ src/components/LandingPage/LoginSignupButton.tsx
@@ -0,0 +1,19 @@
+interface LoginSigninButtonProps {
+  onClick?: () => void;
+}
+
+const LoginSigninButton = ({ onClick }: LoginSigninButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className="
+        flex flex-col items-center justify-center py-2.5 px-5 rounded-full bg-gray-900 shadow-lg cursor-pointer hover:px-[21px] hover:py-[10.5px]"
+    >
+      <span className="text-center text-sm font-bold leading-5 text-white">
+        로그인/회원가입
+      </span>
+    </button>
+  );
+};
+
+export default LoginSigninButton;
