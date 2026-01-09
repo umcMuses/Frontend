c656ca882819d76535c2da8d728856d527b98cbb
Awesome/Lee seung chul <chulee503@naver.com>
2026-01-09 18:02:57 +0900

    style: border white80 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@ const footerLinks = ['서비스 소개', '이용약관', '개인정보처리방�
 
 const Footer = () => {
   return (
-    <footer className="w-full border-t border-[#F3F4F6] bg-white py-16 px-[72.5px]">
+    <footer className="w-full border-t border-white80 bg-white py-16 px-[72.5px]">
       <div className="mx-auto flex w-full max-w-[1280px] flex-col gap-10 px-6">
         <div className="flex justify-center items-start gap-10">
           {footerLinks.map((text) => (
