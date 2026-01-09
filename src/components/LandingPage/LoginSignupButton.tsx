5e85c1c28fb7ac5c1485dc4a8732b1d335c34ddc
Awesome/Lee seung chul <chulee503@naver.com>
2026-01-09 17:35:28 +0900

    style: 로그인/회원가입 버튼색 수정

--- src/components/LandingPage/LoginSignupButton.tsx
+++ src/components/LandingPage/LoginSignupButton.tsx
@@ -2,12 +2,12 @@ interface LoginSigninButtonProps {
   onClick?: () => void;
 }
 
-const LoginSigninButton = ({ onClick }: LoginSigninButtonProps) => {
+const LoginSignupButton = ({ onClick }: LoginSigninButtonProps) => {
   return (
     <button
       onClick={onClick}
       className="
-        flex flex-col items-center justify-center py-2.5 px-5 rounded-full bg-gray-900 cursor-pointer hover:px-[21px] hover:py-[10.5px]"
+        flex flex-col items-center justify-center py-2.5 px-5 rounded-full bg-mainBlack cursor-pointer hover:px-[21px] hover:py-[10.5px]"
     >
       <span className="text-center text-sm font-boldFont text-white">
         로그인/회원가입
@@ -16,4 +16,4 @@ const LoginSigninButton = ({ onClick }: LoginSigninButtonProps) => {
   );
 };
 
-export default LoginSigninButton;
+export default LoginSignupButton;
