f84fabe55eb0542127e1b3f556ab4983b4d0569f
Awesome/Lee seung chul <chulee503@naver.com>
2026-01-13 17:46:06 +0900

    feat: 글씨 애니메이션 추가

--- src/components/LandingPage/LandingBanner.tsx
+++ src/components/LandingPage/LandingBanner.tsx
@@ -1,9 +1,10 @@
 import backgoundcircle from '../../assets/images/backgroundcircle.png';
 import { ArrowRight } from 'lucide-react';
+import FadeIn from './FadeIn';
 
 export default function LandingBanner() {
   return (
-    <section className="relative w-[1440px] h-[675px] flex justify-center">
+    <section className="relative min-h-[675px] flex justify-center">
       <img src={backgoundcircle} alt="Background Circle" className="absolute" />
 
       {/* 내용없는 컨테이너(위치 잡는 용도) */}
@@ -13,29 +14,37 @@ export default function LandingBanner() {
           {/* 내용 */}
           <div className="self-stretch inline-flex flex-col justify-start items-center">
             <h1 className="text-center">
-              <span className="block font-blackFont text-8xl leading-8xl text-mainBlack">
+              <FadeIn delay={200}>
+                <span className="block font-blackFont text-8xl leading-8xl text-mainBlack">
                 Your Space,
               </span>
-              <span className="block font-blackFont text-8xl leading-[108px] bg-gradient-to-r from-solidPurple to-solidBlue bg-clip-text text-transparent">
+              </FadeIn>
+              <FadeIn delay={400}>
+                <span className="block font-blackFont text-8xl leading-[108px] bg-gradient-to-r from-solidPurple to-solidBlue bg-clip-text text-transparent">
                 Our Stage
               </span>
+              </FadeIn>
             </h1>
           </div>
 
-          <div className="w-[672px] max-w-[672px] inline-flex flex-col fustify-start items-center">
-            <p className="text-black80 font-mediumFont text-xl text-center">
+          <FadeIn delay={600}>
+            <div className="w-[672px] max-w-[672px] inline-flex flex-col fustify-start items-center">
+              <p className="text-black80 font-mediumFont text-xl text-center">
               기다렸던 팬들과 만나는 가장 확실한 방법,
               <br />
               당신의 시작을 함께 만들어갑니다.
-            </p>
-          </div>
-
-          <div className="pt-4">
-            <button className="flex gap-2 items-center px-10 py-5 shadow-[0px_8px_10px_-6px_rgba(233,213,255,1.00)] shadow-[0px_20px_25px_-5px_rgba(233,213,255,1.00)] rounded-full bg-mainBlack text-white font-boldFont text-xl cursor-pointer hover:scale-105">
+              </p>
+            </div>
+          </FadeIn>
+          
+          <FadeIn delay={600}>
+            <div className="pt-4">
+              <button className="flex gap-2 items-center px-10 py-5 shadow-[0px_8px_10px_-6px_rgba(233,213,255,1.00)] shadow-[0px_20px_25px_-5px_rgba(233,213,255,1.00)] rounded-full bg-mainBlack text-white font-boldFont text-xl cursor-pointer hover:scale-105">
               프로젝트 시작하기
-              <ArrowRight className="w-5 h-5 relative" />
-            </button>
-          </div>
+                <ArrowRight className="w-5 h-5 relative" />
+              </button>
+            </div>
+          </FadeIn>
         </div>
       </div>
     </section>
--- src/components/LandingPage/PlatformFeatures.tsx
+++ src/components/LandingPage/PlatformFeatures.tsx
@@ -1,65 +1,75 @@
 import safepayment from '../../assets/images/safepayment.png';
 import cheering from '../../assets/images/cheering.png';
 import smartentrysolution from '../../assets/images/smartentrysolution.png';
+import FadeIn from './FadeIn';
 
 export default function PlatformFeatures() {
   return (
-    <section className="w-[1440px] px-[72.5px] py-40 bg-color-white--50%/50 inline-flex flex-col justify-start items-start">
+    <section className="relative w-full px-[72.5px] py-40 bg-color-white--50%/50 inline-flex flex-col justify-center items-center">
       <div className="w-full max-w-[1280px] px-6 flex flex-col justify-start items-start gap-20">
         {/* PlatForm Features */}
         <div className="self-stretch flex flex-col justify-start items-center">
-          <div className="text-center justify-center text-color-azure-17 text-3xl font-boldFont leading-9">
-            Platform Features
-          </div>
+          <FadeIn delay={200}>
+            <div className="text-center justify-center text-color-azure-17 text-3xl font-boldFont leading-9">
+              Platform Features
+            </div>
+          </FadeIn>
         </div>
 
         {/* 카드 배치 */}
-        <div className="self-stretch inline-flex justify-center items-start gap-8">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 justify-items-center">
           {/* Safe Payment */}
-          <div className="w-96 h-[377px] pt-[38.79px] pb-16 flex justify-center items-center gap-10 flex-1 relative bg-white/70 rounded-[40px] shadow-[0px_8px_10px_-6px_rgba(224,231,255,0.50)] shadow-[0px_20px_25px_-5px_rgba(224,231,255,0.50)] outline outline-1 outline-offset-[-1px] outline-white backdrop-blur-md overflow-hidden">
-            <div className="flex flex-col items-center gap-10">
-              <img src={safepayment} className="w-[224px] h-[172px]" />
+          <FadeIn delay={300}>
+            <div className="w-96 h-[377px] pt-[38.79px] pb-16 flex justify-center items-center gap-10 flex-1 relative bg-white/70 rounded-[40px] shadow-[0px_8px_10px_-6px_rgba(224,231,255,0.50)] shadow-[0px_20px_25px_-5px_rgba(224,231,255,0.50)] outline outline-1 outline-offset-[-1px] outline-white backdrop-blur-md overflow-hidden">
+              <div className="flex flex-col items-center gap-10">
+                <img src={safepayment} className="w-[224px] h-[172px]" />
 
-              <div className="flex flex-col items-start">
-                <div className="w-48 h-8 text-mainBlack text-2xl font-boldFont leading-8">
-                  Safe Payment
-                </div>
-                <div className="w-[302px] h-[30px] text-black80 text-lg font-mainFont leading-7">
-                  후원부터 최종 정산까지 모든 결제 과정을 안전하게 보호합니다.
+                <div className="flex flex-col items-start">
+                  <div className="w-48 h-8 text-mainBlack text-2xl font-boldFont leading-8">
+                    Safe Payment
+                  </div>
+                  <div className="w-[302px] h-[30px] text-black80 text-lg font-mainFont leading-7">
+                    후원부터 최종 정산까지 모든 결제 과정을 안전하게 보호합니다.
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </FadeIn>
 
           {/* Cheering */}
-          <div className="w-96 self-stretch relative bg-white/70 rounded-[40px] shadow-[0px_8px_10px_-6px_rgba(224,231,255,0.50)] shadow-[0px_20px_25px_-5px_rgba(224,231,255,0.50)] outline outline-1 outline-offset-[-1px] outline-white backdrop-blur-md overflow-hidden">
-            <img
-              className="w-[355px] h-[222px] left-[13.83px] top-[12.28px] absolute"
-              src={cheering}
-            />
+          <FadeIn delay={400}>
+            <div className="w-96 h-[377px] relative bg-white/70 rounded-[40px] shadow-[0px_8px_10px_-6px_rgba(224,231,255,0.50)] shadow-[0px_20px_25px_-5px_rgba(224,231,255,0.50)] outline outline-1 outline-offset-[-1px] outline-white backdrop-blur-md overflow-hidden">
+              <img
+                className="w-[355px] h-[222px] left-[13.83px] top-[12.28px] absolute"
+                src={cheering}
+              />
 
-            <div className="w-48 h-8 left-[40.84px] top-[250.33px] absolute justify-center text-mainBlack text-2xl font-boldFont leading-8">
-              Cheering
+              <div className="w-48 h-8 left-[40.84px] top-[250.33px] absolute justify-center text-mainBlack text-2xl font-boldFont leading-8">
+                Cheering
+              </div>
+              <div className="w-72 h-7 left-[40.84px] top-[285px] absolute justify-center text-black80 text-lg font-mainFont leading-7">
+                후원과 동시에 전하는 응원 메시지로 끈끈한 유대를 형성합니다.
+              </div>
             </div>
-            <div className="w-72 h-7 left-[40.84px] top-[285px] absolute justify-center text-black80 text-lg font-mainFont leading-7">
-              후원과 동시에 전하는 응원 메시지로 끈끈한 유대를 형성합니다.
-            </div>
-          </div>
+          </FadeIn>
 
           {/* Smart Entry Solution */}
-          <div className="w-96 self-stretch relative bg-white/70 rounded-[40px] shadow-[0px_8px_10px_-6px_rgba(224,231,255,0.50)] shadow-[0px_20px_25px_-5px_rgba(224,231,255,0.50)] outline outline-1 outline-offset-[-1px] outline-white backdrop-blur-md overflow-hidden">
-            <img
-              className="w-[200px] h-[172px] left-[92.33px] top-[37.34px] absolute"
-              src={smartentrysolution}
-            />
+          <FadeIn delay={500}>
+            <div className="w-96 h-[377px] relative bg-white/70 rounded-[40px] shadow-[0px_8px_10px_-6px_rgba(224,231,255,0.50)] shadow-[0px_20px_25px_-5px_rgba(224,231,255,0.50)] outline outline-1 outline-offset-[-1px] outline-white backdrop-blur-md overflow-hidden">
+              <img
+                className="w-[200px] h-[172px] left-[92.33px] top-[37.34px] absolute"
+                src={smartentrysolution}
+              />
 
-            <div className="w-64 h-8 left-[40.84px] top-[250.33px] absolute justify-center text-mainBlack text-2xl font-boldFont leading-8">
-              Smart Entry Solution
+              <div className="w-64 h-8 left-[40.84px] top-[250.33px] absolute justify-center text-mainBlack text-2xl font-boldFont leading-8">
+                Smart Entry Solution
+              </div>
+              <div className="w-72 h-7 left-[40.84px] top-[285px] absolute justify-center text-black80 text-lg font-mainFont leading-7">
+                모바일 QR 시스템 하나로 복잡한 입장 절차를 스마트하게
+                관리하세요.
+              </div>
             </div>
-            <div className="w-72 h-7 left-[40.84px] top-[285px] absolute justify-center text-black80 text-lg font-mainFont leading-7">
-              모바일 QR 시스템 하나로 복잡한 입장 절차를 스마트하게 관리하세요.
-            </div>
-          </div>
+          </FadeIn>
         </div>
       </div>
     </section>
