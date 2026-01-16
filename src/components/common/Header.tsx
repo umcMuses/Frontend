0a9416a8d510007a25c7092ec8f1b3d9e8d28b20
Awesome/Lee seung chul <chulee503@naver.com>
2026-01-17 00:43:26 +0900

    refactor: 오류 import 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
 import museslogo from '../../assets/images/museslogo.png';
-import LoginSignupButton from '../LandingPage/LoginSignupButton';
 
 const Header = () => {
   const navigate = useNavigate();
