546f9c11f9eb47dee7907e7834e475d3d6748ea1
Awesome/Lee seung chul <chulee503@naver.com>
2026-01-17 02:59:34 +0900

    revert: 실수로 건드린 ProjectList.tsx 파일 복구

--- src/components/ProjectListPage/ProjectList.tsx
+++ src/components/ProjectListPage/ProjectList.tsx
@@ -81,10 +81,12 @@ const MOCK_PROJECTS: Project[] = [
 
 export default function ProjectList() {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {MOCK_PROJECTS.map((project) => (
-        <ProjectCard key={project.id} project={project} />
-      ))}
+    <div className="w-full max-w-[1247px]">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {MOCK_PROJECTS.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 }
