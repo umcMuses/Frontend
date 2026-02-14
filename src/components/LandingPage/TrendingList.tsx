a6851515204f597014e974e082d69b40c08cb9a1
sooh329 <glay@kakao.com>
2026-02-14 21:37:40 +0900

    fix: trendingNow css 충돌 수정 및 프롭 수정

--- src/components/LandingPage/TrendingList.tsx
+++ src/components/LandingPage/TrendingList.tsx
@@ -48,7 +48,12 @@ export default function TrendingList({ index }: TrendingListProps) {
             className="w-fit p-5 border border-white80 rounded-[40px] shadow-xs"
           >
             <ProjectCard
-              project={project}
+              project={{
+                ...project,
+                isScheduled: false,
+                opening: '',
+                attachmentImageUrl: null,
+              }}
               posterClassNameValue="h-[422.5px] w-[338px] rounded-[32px] group-hover:shadow-lg transition-all"
               contentClassNameValue="group-hover:-translate-y-[-8px] transition-all"
             />
