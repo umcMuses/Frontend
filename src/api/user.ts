b86600befb08b560d7bfd0e944577b77236767f7
Awesome/Lee seung chul <chulee503@naver.com>
2026-02-15 17:23:40 +0900

    refactor: axois를 인증된 api방식 axiosInstance로 변경

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,15 +1,15 @@
-import axios from 'axios';
 import { ENDPOINTS } from './endpoints';
 import type { Member } from '../components/MyPage/types/apitypes/members';
+import api from './axiosInstance';
 
 export const getMyInfo = async () => {
-  const res = await axios.get(ENDPOINTS.MY_INFO, {
+  const res = await api.get(ENDPOINTS.MY_INFO, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
     },
   });
 
-  return res.data.data; // 🔥 이게 핵심
+  return res.data.data;
 };
 
 export const updateMyProfile = async (payload: {
@@ -18,14 +18,9 @@ export const updateMyProfile = async (payload: {
   birthday: string;
   gender: number;
 }) => {
-  const res = await axios.post(
+  const res = await api.post(
     ENDPOINTS.MY_PROFILE_UPDATE, // "/api/users/me/profile"
-    payload,
-    {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-      },
-    }
+    payload
   );
 
   return res.data.data as Member;
